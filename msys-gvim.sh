dd66ad922dd62032579b1195e0bf4f4a1d90beac
James Hillyerd <jamehi03@noa.nintendo.com>
2016-03-30 12:08:02 -0700

    Update cyg-gvim for msys (git for windows)

--- msys-gvim.sh
+++ msys-gvim.sh
@@ -1,8 +1,13 @@
+# msys-gvim.sh
+# description: Launches an instance of GVim (standard Windows build) from
+# within an msys-bash shell.  Posix paths will be mapped to Windows paths.
+# You must opt for the command line .bat files when installing Gvim.
+#
 # Edit the HOME= line below to map to your desired "home" for Vim,
 # if you don't want the Windows default.
 
 (
-  for f in "$@"; do 
+  for f in "$@"; do
     case $f in
       -*) echo $f;;
       *) cygpath -w "$f";;
@@ -18,4 +23,4 @@
     LOCALAPPDATA="$LOCALAPPDATA" \
     COMSPEC="$COMSPEC" \
     SYSTEMROOT="$SYSTEMROOT" \
-    "/cygdrive/c/Windows/gvim.bat" $ARGS
+    "/c/Windows/gvim.bat" $ARGS
