62f5f5e0407a6e0892ffe989bb16c8fb9825660c
James Hillyerd <james@hillyerd.com>
2023-01-15 12:53:58 -0800

    telescope: delete buffer mapping

--- lua/config.lua
+++ lua/config.lua
@@ -106,11 +106,23 @@ end
 
 -- Plugin: Telescope
 do
+  require('telescope').setup {
+    pickers = {
+      buffers = {
+        mappings = {
+          n = {
+            ["dd"] = require('telescope.actions').delete_buffer,
+          },
+        },
+      },
+    },
+  }
+
   local builtin = require('telescope.builtin')
   vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+  vim.keymap.set('n', '<leader>b', builtin.buffers, {})
   vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
   vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-  vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
   vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
   vim.keymap.set('n', '<leader>fl', builtin.current_buffer_fuzzy_find, {})
 end
@@ -122,6 +134,7 @@ do
     insert_mappings = false,
     terminal_mappings = false,
 
+    -- auto_scroll = true,
     direction = 'vertical',
     size = vim.o.columns * 0.4,
   }
