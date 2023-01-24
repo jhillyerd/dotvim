f020bf4452ef9eeef6fcc283f664b73c8d039b6d
James Hillyerd <james@hillyerd.com>
2023-01-24 08:46:35 -0800

    Add mapping for telescope lsp diagnostics

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -13,6 +13,7 @@ require('telescope').setup {
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
