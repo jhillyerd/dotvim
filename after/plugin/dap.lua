0159904c1fa3e3d601c53faba23cefeb04832f44
James Hillyerd <james@hillyerd.com>
2023-02-02 13:21:03 -0800

    Don't remap enter for dap

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -7,11 +7,12 @@ dap.listeners.after.event_initialized["dapui_config"] = dapui.open
 dap.listeners.before.event_terminated["dapui_config"] = dapui.close
 dap.listeners.before.event_exited["dapui_config"] = dapui.close
 
-vim.keymap.set("n", "<Enter>", dap.step_over)
-vim.keymap.set("n", "<LocalLeader>dd", dap.continue) -- also starts dap.
+vim.keymap.set("n", "<F2>", dap.step_into)
+vim.keymap.set("n", "<F3>", dap.step_over)
+vim.keymap.set("n", "<F4>", dap.step_out)
+vim.keymap.set("n", "<F5>", dap.continue) -- also starts dap.
 vim.keymap.set("n", "<LocalLeader>db", dap.toggle_breakpoint)
-vim.keymap.set("n", "<LocalLeader>di", dap.step_into)
-vim.keymap.set("n", "<LocalLeader>do", dap.step_out)
+vim.keymap.set("n", "<LocalLeader>dl", dap.run_to_cursor)
 vim.keymap.set("n", "<LocalLeader>dq", function() -- quit dap.
   dap.disconnect()
   dap.close()
