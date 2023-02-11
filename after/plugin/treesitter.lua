94433f414a14c6231a1138dc5f291f6a2310b4ff
James Hillyerd <james@hillyerd.com>
2023-02-10 17:11:30 -0800

    Prevent treesitter crash

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -9,6 +9,9 @@ require("nvim-treesitter.configs").setup {
         ["af"] = "@function.outer",
         ["if"] = "@function.inner",
       },
+      -- Prevent nvim-treesitter/ts_utils.lua:285: Column value outside range
+      -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects/issues/377
+      include_surrounding_whitespace = false,
     },
   },
 }
