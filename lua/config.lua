5354a3a520f4464a39a63548b0c664932491c270
James Hillyerd <james@hillyerd.com>
2023-11-14 11:23:30 -0800

    Make toggleterm optional

--- lua/config.lua
+++ lua/config.lua
@@ -100,25 +100,28 @@ end
 
 -- Plugin: ToggleTerm
 do
-  require('toggleterm').setup{
-    open_mapping = [[<leader>t]],
-    insert_mappings = false,
-    terminal_mappings = false,
+  local ok, toggleterm = pcall(require, 'toggleterm')
+  if ok then
+    toggleterm.setup{
+      open_mapping = [[<leader>t]],
+      insert_mappings = false,
+      terminal_mappings = false,
 
-    -- auto_scroll = true,
-    direction = 'vertical',
-    size = vim.o.columns * 0.4,
-  }
+      -- auto_scroll = true,
+      direction = 'vertical',
+      size = vim.o.columns * 0.4,
+    }
 
-  local Terminal = require("toggleterm.terminal").Terminal
+    local Terminal = require("toggleterm.terminal").Terminal
 
-  -- lazygit in a floating terminal.
-  local lazygit = Terminal:new({
-    cmd = "lazygit",
-    direction = "float",
-    hidden = true,
-  })
-  vim.keymap.set("n", "<leader>gg", function() lazygit:toggle() end, map_opts)
+    -- lazygit in a floating terminal.
+    local lazygit = Terminal:new({
+      cmd = "lazygit",
+      direction = "float",
+      hidden = true,
+    })
+    vim.keymap.set("n", "<leader>gg", function() lazygit:toggle() end, map_opts)
+  end
 end
 
 -- Language: Rust
