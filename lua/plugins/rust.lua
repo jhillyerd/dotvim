8aba2e4917d8d38d31edabb231b64e83212849e9
James Hillyerd <james@hillyerd.com>
2024-09-23 15:07:08 -0700

    rust: disable checkOnSave

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -2,4 +2,17 @@ return {
   "mrcjkb/rustaceanvim",
   version = '^4',
   lazy = false, -- This plugin is already lazy
+  init = function()
+    vim.g.rustaceanvim = {
+      -- LSP configuration
+      server = {
+        default_settings = {
+          -- rust-analyzer language server configuration.
+          ['rust-analyzer'] = {
+            checkOnSave = false,
+          },
+        },
+      },
+    }
+  end,
 }
