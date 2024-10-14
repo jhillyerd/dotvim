ddb65644d7373ea6299168e231b30fef857a3964
James Hillyerd <james@hillyerd.com>
2024-10-14 11:36:15 -0700

    rust: ,cc -> flyCheck

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,5 +1,7 @@
 local map_opts = { noremap = true, silent = true }
 
+vim.keymap.set("n", "<LocalLeader>cc", ":RustLsp flyCheck<Cr>", map_opts)
+
 vim.keymap.set("n", "<LocalLeader>t", function()
   vim.notify("Fetching testables...")
   vim.cmd.RustLsp({ "testables", bang = true })
