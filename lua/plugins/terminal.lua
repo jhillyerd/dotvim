8d75a7dcb642c214f285560326a99edda4caab59
James Hillyerd <james@hillyerd.com>
2025-06-06 15:09:24 -0700

    more toggleterm mappings

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,11 +1,19 @@
+local trim_spaces = true
+local set_opfunc = vim.fn[vim.api.nvim_exec([[
+  func s:set_opfunc(val)
+    let &opfunc = a:val
+  endfunc
+  echon get(function('s:set_opfunc'), 'name')
+]], true)]
+
 return {
   "akinsho/toggleterm.nvim",
 
-  version = "^2.3.0",
+  version = "^2.13.0",
 
   config = function()
     require("toggleterm").setup({
-      open_mapping = "<leader>t",
+      open_mapping = "<Leader>tt",
       insert_mappings = false,
       terminal_mappings = false,
 
@@ -15,5 +23,18 @@ return {
 
       shell = "fish",
     })
+
+    vim.keymap.set("n", "<Leader>ts", function()
+      require("toggleterm").send_lines_to_terminal("single_line", trim_spaces, { args = vim.v.count })
+    end)
+    vim.keymap.set("v", "<Leader>ts", function()
+      require("toggleterm").send_lines_to_terminal("visual_selection", trim_spaces, { args = vim.v.count })
+    end)
+    vim.keymap.set("n", "<Leader>tf", function()
+      set_opfunc(function(motion_type)
+        require("toggleterm").send_lines_to_terminal(motion_type, false, { args = vim.v.count })
+      end)
+      vim.api.nvim_feedkeys("ggg@G''", "n", false)
+    end)
   end
 }
