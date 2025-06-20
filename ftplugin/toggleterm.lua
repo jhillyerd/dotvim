3506bab4f6a0719c9e968f412d7f818b48c4b369
James Hillyerd <james@hillyerd.com>
2025-06-20 15:47:27 -0700

    toggleterm insert mapping

--- ftplugin/toggleterm.lua
+++
@@ -1,4 +0,0 @@
-local map_opts = { buffer = 0, noremap = true, silent = true }
-
--- Kills terminal session, closes window.
-vim.keymap.set("n", "q", ":<C-u>q!<Cr>", map_opts)
--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -16,6 +16,7 @@ return {
       open_mapping = "<Leader>tt",
       insert_mappings = false,
       terminal_mappings = false,
+      persist_mode = false, -- Breaks insert mapping.
 
       -- auto_scroll = true,
       direction = "horizontal",
@@ -43,6 +44,41 @@ return {
         require("toggleterm").send_lines_to_terminal(motion_type, false, { args = vim.v.count })
       end)
       vim.api.nvim_feedkeys("g@ap", "n", false)
+    end)
+
+    -- Keymap to jump to current toggleterm buffer and enter insert mode.
+    vim.keymap.set("n", "<Leader>ti", function()
+      -- List current buffers.
+      local buffers = vim.api.nvim_list_bufs()
+
+      -- Check if toggleterm buffer exists.
+      local toggleterm_bufnr = false
+      for _, buf in ipairs(buffers) do
+        local buf_name = vim.api.nvim_buf_get_name(buf)
+        if buf_name:find("toggleterm#") then
+          toggleterm_bufnr = buf
+          break
+        end
+      end
+
+      local toggleterm_winid = false
+      if toggleterm_bufnr then
+        -- Find toggleterm window.
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+          if vim.api.nvim_win_get_buf(win) == toggleterm_bufnr then
+            toggleterm_winid = win
+            break
+          end
+        end
+      end
+
+      -- Switch to toggleterm window, or start new one.
+      if toggleterm_winid then
+        vim.api.nvim_set_current_win(toggleterm_winid)
+        vim.cmd("startinsert!")
+      else
+        vim.cmd([[ exe 1 . "ToggleTerm" ]])
+      end
     end)
   end
 }
